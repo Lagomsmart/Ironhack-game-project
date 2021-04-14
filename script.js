0d9d09b27ba8858b60b54755976a89f419012d19
Jesper <jesperbennett@protonmail.com>
2021-04-14 15:48:20 -0400

    wasd

--- script.js
+++ script.js
@@ -161,9 +161,9 @@ class Enemy {
         this.rowImOn = 1
         this.enemyImg = enemyImg
         this.sx = 0
-        this.sy = this.rowImOn * (this.enemyImg.height / this.numberOfRows)
-        this.sw = this.img.width / this.numberOfImages
-        this.sh = this.img.height / this.numberOfRows
+        //this.sy = this.rowImOn * (this.enemyImg.height / this.numberOfRows)
+        //this.sw = this.img.width / this.numberOfImages
+        //this.sh = this.img.height / this.numberOfRows
         this.dx = 0
         this.dy = 0
     }
@@ -180,7 +180,7 @@ class Enemy {
         this.x = this.x// + this.velocity.x
         this.y = this.y// + this.velocity.y
     }
-    move() {
+    move() { // Enemy following player
         if (player.x > this.x) {
             this.x += this.speed
         }
@@ -194,7 +194,7 @@ class Enemy {
             this.y -= this.speed
         }
     }
-    randomPathing() { //TESTING    Enemy random pathing left
+    randomPathing() { // Enemy random pathing left
         let randomNum = Math.floor(Math.random() * 2)
         this.x -= this.speed
         if (randomNum === 1) {
@@ -257,8 +257,8 @@ let defaultPlayerY = canvas.height / 2
 let img = new Image()
 img.src = './images/player.png';
 
-
 const player = new Player(defaultPlayerX, defaultPlayerY, img.width, img.height, 5, 100, 100, 5, 100, 100, img) //(x, y, w, h, speed, maxhealth, health, damage, stamina, maxStamina)
+
 
 
 
@@ -362,9 +362,10 @@ setInterval(() => {
 }, 1000)
 
 
+// Spawn enemies every X seconds
+setInterval(() => {
 
-
-
+}, 1000)
 
 
 setInterval(() => {
@@ -378,20 +379,22 @@ setInterval(() => {
 
 
 
-
-
-
 if (powerups.length < 1) {
     powerups.push(healthPotion)
 }
 
-// for (let i = otherEnemies.length; i < maxAmountOfOtherEnemies; i++) {
-//     otherEnemies.push(new Enemy(1150, Math.random() * 450 + 100, 50, 50, 1, 'red', 10, 1))
-// }
-// for (let i = enemies.length; i < maxAmountOfEnemies; i++) {
-//     enemies.push(new Enemy(Math.random() * 1000 + 200, Math.random() * 650, 50, 50, 1, 'blue', 10, 1))
-// }
 
+
+// ---------- INITIAL ENEMY SPAWNS - ONLY ONCE  ----------
+
+window.onload = function () {
+    for (let i = otherEnemies.length; i < maxAmountOfOtherEnemies; i++) {
+        otherEnemies.push(new Enemy(1350, Math.random() * 450 + 100, 50, 50, 1, 'red', 10, 1))
+    }
+    for (let i = enemies.length; i < maxAmountOfEnemies; i++) {
+        enemies.push(new Enemy(Math.random() * 1000 + 200, Math.random() * 650, 50, 50, 1, 'blue', 10, 1))
+    }
+}
 
 
 ctx.drawImage(player.img, player.x, player.y)
@@ -430,15 +433,16 @@ function animate() {
     })
 
 
-    //  CREATING/PUSHING THE ENEMIES //
+
+    //  ----- CREATING/PUSHING THE ENEMIES  constantly   //
+    // if (enemies.length < maxAmountOfEnemies) {
+    //     enemies.push(new Enemy(Math.random() * 1000 + 200, Math.random() * 650, 50, 50, 1, 'blue', 10, 1))
+    // }
+    // if (otherEnemies.length < maxAmountOfOtherEnemies) {
+    //     otherEnemies.push(new Enemy(1150, Math.random() * 450 + 100, 50, 50, 1, 'red', 10, 1))
+    // }
 
 
-    if (enemies.length < maxAmountOfEnemies) {
-        enemies.push(new Enemy(Math.random() * 1000 + 200, Math.random() * 650, 50, 50, 1, 'blue', 10, 1))
-    }
-    if (otherEnemies.length < maxAmountOfOtherEnemies) {
-        otherEnemies.push(new Enemy(1150, Math.random() * 450 + 100, 50, 50, 1, 'red', 10, 1))
-    }
 
     // [enemies] moving
     enemies.forEach((enemy) => {
@@ -456,7 +460,7 @@ function animate() {
 
     });
 
-    // [enemies] updating   BLUE
+    // [enemies] updating   BLUE ZOMBIE
     enemies.forEach((enemy, index) => {
         enemy.update(
 
@@ -470,7 +474,7 @@ function animate() {
         )
     })
 
-    // [otherEnemies] updating  RED
+    // [otherEnemies] updating  RED FIREBALL
     otherEnemies.forEach((enemy, index) => {
         enemy.update(
 
@@ -497,7 +501,7 @@ function animate() {
 
 
     //If player reaches next door
-    if (player.x == canvas.width - 50 && player.y < 350 && player.y > 250 && otherEnemies.length < 1 && enemies.length < 1) {
+    if (player.x == canvas.width - player.w && player.y < 350 && player.y > 250 && otherEnemies.length < 1 && enemies.length < 1) {
         console.log('next room!');
         let currentLevel = 1
 
